241dc4a70b08202b2cbb8ef62c19c9afbcfa3959
SatyendraRock <125202359+SatyendraRock@users.noreply.github.com>
2025-11-03 11:31:34 +0530

    Add payment verification and failure routes

--- store/urls.py
+++ store/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('order-history/', views.order_history, name='order_history'),
+    path('verify-payment/', views.verify_payment, name='verify_payment'),
+    path('payment-failed/', views.payment_failed, name='payment_failed'),
 
 
 ]
